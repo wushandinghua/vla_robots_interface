e67d4ff248d082a6b5a235bcd2749cc950fa5c25
wushandinghua <quebinbin666@163.com>
2025-10-13 16:41:54 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name="vri",  
     version="0.1.0",  
-    packages=["vri"],  
+    # packages=["vri"], 
+    packages=find_packages(), 
+    install_requires=[
+        'flask==3.1.1',
+        'imageio==2.37.0',
+        'imageio-ffmpeg==0.6.0',
+        'opencv-python==4.12.0.88',
+        'Pillow==11.3.0',
+        'einops==0.8.0',
+        'matplotlib==3.10.0',
+        'dm_env==1.6',
+        'websocket-client==1.9.0',
+        'msgpack==1.1.0',
+        'pyudev==0.24.3'
+    ],
 )
